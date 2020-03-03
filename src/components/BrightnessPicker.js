399d78de73324df6a7c880b4a67091544b9deafa
Martin Ždila <m.zdila@gmail.com>
2020-03-03 21:56:30 +0100

    fixed hook dependency

--- src/components/BrightnessPicker.js
+++ src/components/BrightnessPicker.js
@@ -36,7 +36,7 @@ export function BrightnessPicker({ onChange, color }) {
         sendEvent(e);
       }
     },
-    [sendEvent],
+    [sendEvent, dragging],
   );
 
   const brightnessPickerStyle = useMemo(
